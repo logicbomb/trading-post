3d2511103f37b283ce83a22e5dd13148aee78723
Jason Turim <jason@signalvine.com>
2014-01-17 16:18:37 -0700

    Firebase wireup

--- assets/js/trading-post.js
+++ assets/js/trading-post.js
@@ -1,13 +1,23 @@
-var app = angular.module("tradingpost",["ngRoute"]);
+var app = angular.module("tradingpost",["ngRoute", "firebase"]);
+
+app.constant("FIREBASE_URL", "https://scorching-fire-2975.firebaseio.com/");
 
 app.config(["$routeProvider", function($routeProvider) {
-  console.log("config is happening");
   $routeProvider
     .when("/", { controller: 'homeCtl', templateUrl: '/view/home.html'})
     .otherwise({ redirectTo: "/" });
 }]);
 
-app.controller("homeCtl", ["$scope", function($scope) {
-  console.log("homeCtl is happening");
-  $scope.name = "Hello";
+app.controller("homeCtl", ["$scope", "$firebase", "FIREBASE_URL", function($scope, $firebase, FIREBASE_URL) {
+  var ref = new Firebase(FIREBASE_URL);
+  $scope.users = $firebase(ref);
+
+  $scope.addUser = function() {
+    $scope.users.$add({
+      name: "User " +  Math.floor(Math.random() * 100),
+      location:  Math.floor(Math.random() * 100),
+      prize:  Math.floor(Math.random() * 100),
+      want: [ Math.floor(Math.random() * 100), Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)]
+    });
+  };
 }]);
