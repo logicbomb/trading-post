230ab98ece12f882143c99e1801adbf60b5e36dc
Jason Turim <jason@signalvine.com>
2014-01-17 14:55:17 -0700

    Initial commit - server & directory structure

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+app.use(express.bodyParser());
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+
+app.configure(function(){
+  app.use('/css', express.static(__dirname + '/assets/css'));
+  app.use('/js', express.static(__dirname + '/assets/js'));
+  app.use(express.static(__dirname + '/app'));
+});
+
+app.listen(3000);
