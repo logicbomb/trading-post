2c484a7cdb6356494771be6c1a4a5f970a273451
Jason Turim <jason@signalvine.com>
2014-01-17 20:44:30 -0700

    Initial add-item screen

--- assets/js/trading-post.js
+++ assets/js/trading-post.js
@@ -5,18 +5,27 @@ app.constant("FIREBASE_URL", "https://scorching-fire-2975.firebaseio.com")
 app.config(["$routeProvider", function($routeProvider) {
   $routeProvider
     .when("/", { controller: 'homeCtl', templateUrl: '/view/home.html'})
+    .when("/add-item", { controller: 'addCtl', templateUrl: '/view/new-item.html'})
     .otherwise({ redirectTo: "/" });
 }]);
 
-app.controller("homeCtl", ["$scope", "$firebase", "uuid", "FIREBASE_URL", function($scope, $firebase, uuid, FIREBASE_URL) {
-  $scope.url = FIREBASE_URL;
+app.factory('tradingService', ["$firebase", "FIREBASE_URL", function($firebase, FIREBASE_URL) {
   var baseRef = new Firebase(FIREBASE_URL);
-  var userRef = baseRef.child("users");
-  $scope.users = $firebase(userRef);
-  $scope.wants = baseRef.child("items");
+  var u = baseRef.child("users");
+  var i = baseRef.child("items");
 
+  return {
+    userRef: u,
+    itemRef: i,
+
+    getUsers: function() { return $firebase(u); },
+    getItems: function() { return $firebase(i); }
+  };
+}]);
+
+app.controller("homeCtl", ["$scope", "uuid", "tradingService", function($scope, uuid, tradingService) {  
   $scope.addUser = function() {
-    userRef.push(
+    tradingService.userRef.push(
       { 
         name: "User " +  Math.floor(Math.random() * 100),
         location:  Math.floor(Math.random() * 100),
@@ -25,4 +34,20 @@ app.controller("homeCtl", ["$scope", "$firebase", "uuid", "FIREBASE_URL", functi
       }
     );
   };
+}]);
+
+app.controller("addCtl", ["$scope", "uuid", "tradingService", function($scope, uuid, tradingService) {
+  $scope.reset = function() {
+    $scope.item = {
+      name: '',
+      price: '',
+      url: ''
+    };
+  }
+
+  $scope.addItem = function() {
+    tradingService.itemRef.push($scope.item);
+  }
+
+  $scope.reset();
 }]);
